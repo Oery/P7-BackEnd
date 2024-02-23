5f8643036aeef69837ec76e3fd31c37d7df76e1d
Oery <48154940+Oery@users.noreply.github.com>
2024-02-23 14:42:12 +0100

    add main

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import Mongoose from 'mongoose';
+import bookRoutes from './routes/book.js';
+import userRoutes from './routes/user.js';
+import cors from 'cors';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const corsSettings = {
+    // origin: 'http://localhost:3000',
+    origin: '*',
+};
+
+const app = express();
+
+Mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log('MongoDB Connected'))
+    .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(cors(corsSettings));
+app.use('/api/books', bookRoutes);
+app.use('/api/auth', userRoutes);
+
+app.listen(4000);
