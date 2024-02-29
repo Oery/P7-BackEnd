012b84212efc07164facbe59750849d0aba6dd10
Oery <48154940+Oery@users.noreply.github.com>
2024-02-29 16:04:15 +0100

    refacto

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,14 +4,13 @@ import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res) => {
     bcrypt.hash(req.body.password, 10, async (err, hash) => {
-        if (err) {
-            res.status(500).json({ err });
-            return;
-        }
+        if (err) return res.status(500).json({ err });
+
         const user = new User({
             email: req.body.email,
             password: hash,
         });
+
         user.save()
             .then(() => res.json({ message: 'Utilisateur crée !' }))
             .catch((err) => res.status(400).json({ err }));
@@ -20,23 +19,16 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-        res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
-        return;
-    }
+    if (!user) return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
 
     bcrypt.compare(req.body.password, user.password, (err, result) => {
-        if (err) {
-            res.status(500).json({ err });
-            return;
-        }
-        if (result) {
-            res.json({
-                userId: user._id,
-                token: jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', { expiresIn: '24h' }),
-            });
-        } else {
-            res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
-        }
+        if (err) return res.status(500).json({ err });
+        if (!result)
+            return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+
+        return res.json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, 'RANDOM_TOKEN_SECRET', { expiresIn: '24h' }),
+        });
     });
 };
