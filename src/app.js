3707544fd90bd81cd9d7afdb50d204c40ac9c041
Oery <48154940+Oery@users.noreply.github.com>
2024-03-01 14:06:25 +0100

    add: static folder for images

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import Mongoose from 'mongoose';
 import bookRoutes from './routes/book.js';
 import userRoutes from './routes/user.js';
 import cors from 'cors';
-import path from 'path';
 
 import dotenv from 'dotenv';
 dotenv.config();
@@ -18,7 +17,7 @@ Mongoose.connect(process.env.MONGODB_URL)
     .then(() => console.log('MongoDB Connected'))
     .catch((err) => console.log(err));
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.static('public'));
 app.use(express.json());
 app.use(cors(corsSettings));
 app.use('/api/books', bookRoutes);
