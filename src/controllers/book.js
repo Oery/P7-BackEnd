71162f174265409991c3416d51802c10003b9679
Oery <48154940+Oery@users.noreply.github.com>
2024-03-01 14:17:51 +0100

    feat: image upload handling with compression

--- src/controllers/book.js
+++ src/controllers/book.js
@@ -1,20 +1,35 @@
+import sharp from 'sharp';
 import Book from '../models/book.js';
+import fs from 'fs';
+
+// req.book is set by validateBook middleware in middleware/book.js
+// req.auth is set by auth middleware in middleware/auth.js
 
 export const createBook = async (req, res) => {
-    // TODO: Handle image upload with Multer
-    // const { book, image } = req.body; // book is a string, image is a file
+    const book = JSON.parse(req.body.book);
 
-    // const newBook = new Book({
-    //     title: req.body.title,
-    //     author: req.body.author,
-    //     year: req.body.year,
-    //     genre: req.body.genre,
-    //     userId: req.body.userId,
-    //     ratings: [],
-    //     averageRating: 0,
-    // });
-    // await newBook.save();
-    res.json(newBook);
+    sharp(req.file.path)
+        .resize(800)
+        .jpeg({ quality: 80, chromaSubsampling: '4:2:0' })
+        .toFile(`public/images/${req.file.filename}`)
+        .then(() => {
+            fs.unlink(req.file.path, (err) => {
+                if (err) throw err;
+                console.log('Image compressed and original deleted');
+            });
+        })
+        .catch((error) => console.log(error));
+
+    const newBook = new Book({
+        ...book,
+        userId: req.auth.userId,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    });
+
+    newBook
+        .save()
+        .then(() => res.status(201).json({ message: 'Book created' }))
+        .catch((error) => res.status(400).json({ error }));
 };
 
 export const getBooks = async (_, res) => {
@@ -31,13 +46,13 @@ export const deleteBook = async (req, res) => {
         return res.status(403).send('Forbidden');
     }
 
-    if (req.auth.userId !== book.userId) {
-        res.status(403).send('Forbidden');
-        return;
-    }
-
-    await Book.findByIdAndDelete(req.params.id);
-    res.json({ message: 'Book deleted' });
+    const filename = req.book.imageUrl.split('/images/')[1];
+    fs.unlink(`public/images/${filename}`, () => {
+        req.book
+            .deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Book deleted' }))
+            .catch((error) => res.status(401).json({ error }));
+    });
 };
 
 export const rateBook = async (req, res) => {
@@ -57,6 +72,40 @@ export const rateBook = async (req, res) => {
 };
 
 export const updateBook = async (req, res) => {
+    if (req.auth.userId !== req.book.userId) {
+        return res.status(403).send('Forbidden');
+    }
+
+    if (!req.file) {
+        await Book.updateOne({ _id: req.params.id }, { ...req.body });
+        return res.status(200).send('Book updated');
+    }
+
+    const book = JSON.parse(req.body.book);
+
+    sharp(req.file.path)
+        .resize(800)
+        .jpeg({ quality: 80, chromaSubsampling: '4:2:0' })
+        .toFile(`public/images/${req.file.filename}`)
+        .then(() => {
+            fs.unlink(req.file.path, (err) => {
+                if (err) throw err;
+                console.log('Image compressed and original deleted');
+            });
+        })
+        .catch((error) => console.log(error));
+
+    const oldImage = req.book.imageUrl.split('/images/')[1];
+    fs.unlink(`public/images/${oldImage}`, () => {
+        console.log('Old image deleted');
+    });
+
+    const newBook = {
+        ...book,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
+    };
+
+    await Book.updateOne({ _id: req.params.id }, { ...newBook });
     return res.status(200).send('Book updated');
 };
 
