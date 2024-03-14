64a551e3a429d0e07d6595d1472a188970f3718a
Oery <48154940+Oery@users.noreply.github.com>
2024-03-14 17:21:21 +0100

    add: temp folder reset

--- src/app.js
+++ src/app.js
@@ -3,9 +3,16 @@ import Mongoose from 'mongoose';
 import bookRoutes from './routes/book.js';
 import userRoutes from './routes/user.js';
 import cors from 'cors';
+import fs from 'fs';
 
 import dotenv from 'dotenv';
 dotenv.config();
+
+// Clean temp folder
+const files = fs.readdirSync('temp');
+files.forEach((file) => {
+    fs.unlinkSync(`temp/${file}`);
+});
 
 const corsSettings = {
     origin: process.env.APP_URL,
