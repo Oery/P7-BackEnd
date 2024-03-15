fb833ba39d44ca9254245151d06334f294253fe0
Oery <48154940+Oery@users.noreply.github.com>
2024-03-15 14:03:07 +0100

    add: tryAuth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -11,3 +11,15 @@ export const auth = (req, res, next) => {
         res.status(401).json({ error });
     }
 };
+
+export const tryAuth = (req, _, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
+        req.auth = { userId };
+        next();
+    } catch (error) {
+        next();
+    }
+};
