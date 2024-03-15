ea0064a55fa0528852c149c7616ad0514864bb33
Oery <48154940+Oery@users.noreply.github.com>
2024-03-15 16:52:10 +0100

    refactor: extracted login logic

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,16 @@
 import jwt from 'jsonwebtoken';
 
+function login(req) {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+    const userId = decodedToken.userId;
+    return { userId };
+}
+
 // Adds user data if logged in AND enforces it
 export const auth = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-        const userId = decodedToken.userId;
-        req.auth = { userId };
+        req.auth = login(req);
         next();
     } catch (error) {
         res.status(401).json(error);
@@ -16,10 +20,7 @@ export const auth = (req, res, next) => {
 // Adds user data if logged in but DOES NOT enforce it
 export const tryAuth = (req, _, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-        const userId = decodedToken.userId;
-        req.auth = { userId };
+        req.auth = login(req);
         next();
     } catch (error) {
         next();
